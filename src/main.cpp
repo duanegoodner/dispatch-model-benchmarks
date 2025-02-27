fc042c8d55bc2de734f0b4ae1ba091c3ab6a6b40
Duane Goodner <dmgoodner@gmail.com>
2025-02-26 23:46:21 -0700

    update --help info

--- src/main.cpp
+++ src/main.cpp
@@ -15,14 +15,14 @@ void PrintUsage(const char *program_name) {
             << " [polymorphism_category] [computation]\n"
             << " - No arguments: Runs all tests.\n"
             << " - With arguments: Runs a specific test.\n"
-            << "   Valid categories: ";
+            << "   Valid values for polymorphism_category: ";
 
   const auto &test_case_map = test_runner::GetTestCaseMap();
   for (const auto &category : test_case_map) {
     std::cerr << category.first << " ";
   }
 
-  std::cerr << "\n   Valid computations: ";
+  std::cerr << "\n   Valid values for computation: ";
   if (!test_case_map.empty()) {
     for (const auto &computation : test_case_map.begin()->second) {
       std::cerr << computation.first << " ";
