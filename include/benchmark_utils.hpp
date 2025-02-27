3cda65333245f1b799554cc0314c9626b6510bd9
Duane Goodner <dmgoodner@gmail.com>
2025-02-26 19:07:49 -0700

    change return type of RunBenchMark to  std::chrono::duration<double>

--- include/benchmark_utils.hpp
+++ include/benchmark_utils.hpp
@@ -12,7 +12,7 @@ void PrintTime(const std::string &label, std::chrono::duration<double> elapsed);
 
 // Generalized benchmarking function
 template <typename Callable>
-void RunBenchmark(const std::string &label, size_t n, Callable &&compute_func) {
+std::chrono::duration<double> RunBenchmark(const std::string &label, size_t n, Callable &&compute_func) {
   auto start = std::chrono::high_resolution_clock::now();
   double sum = 0.0;
   for (size_t i = 0; i < n; ++i) {
@@ -20,5 +20,8 @@ void RunBenchmark(const std::string &label, size_t n, Callable &&compute_func) {
   }
   prevent_optimization = sum; // don't let compiler optimize out our test loop
   auto end = std::chrono::high_resolution_clock::now();
-  PrintTime(label, end - start);
+  auto elapsed = end - start;
+  PrintTime(label, elapsed);
+  
+  return elapsed;
 }
