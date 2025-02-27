486f612bd22a5048d144e9e75bb88229af94c4db
Duane Goodner <dmgoodner@gmail.com>
2025-02-26 18:53:37 -0700

    fix minor styling issue

--- include/benchmark_utils.hpp
+++ include/benchmark_utils.hpp
@@ -12,11 +12,11 @@ void PrintTime(const std::string &label, std::chrono::duration<double> elapsed);
 
 // Generalized benchmarking function
 template <typename Callable>
-void RunBenchmark(const std::string &label, size_t n, Callable &&computeFunc) {
+void RunBenchmark(const std::string &label, size_t n, Callable &&compute_func) {
   auto start = std::chrono::high_resolution_clock::now();
   double sum = 0.0;
   for (size_t i = 0; i < n; ++i) {
-    sum += computeFunc(2.0);
+    sum += compute_func(2.0);
   }
   prevent_optimization = sum; // don't let compiler optimize out our test loop
   auto end = std::chrono::high_resolution_clock::now();
