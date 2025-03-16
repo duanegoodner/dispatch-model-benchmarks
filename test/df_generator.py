288677353ac1e28782991f2ba89998febe0aceec
Duane Goodner <dmgoodner@gmail.com>
2025-03-15 18:32:21 -0600

    update datframe col label format

--- test/df_generator.py
+++ test/df_generator.py
@@ -16,12 +16,13 @@ def parse_benchmark_results(output):
     if matches:
         test_name, iteration_count = matches[0]
         benchmark_summary["Test Name"] = test_name
-        benchmark_summary["Mean Iteration Count"] = int(iteration_count)
+        benchmark_summary["Mean Iteration Count (Counts)"] = int(iteration_count)
 
     # Extract mean time and error percentage from the last part
     time_pattern = re.search(r"(\d+\.\d+) \+\- (\d+\.\d+) seconds time elapsed  \( \+\-\s*(\d+\.\d+)% \)", output)
     if time_pattern:
-        benchmark_summary["Mean Time (seconds)"] = (float(time_pattern.group(1)), float(time_pattern.group(3)))
+        benchmark_summary["Mean Time (seconds)"] = float(time_pattern.group(1))
+        benchmark_summary["Time (% error)"] = float(time_pattern.group(3))
 
     return benchmark_summary
 
@@ -36,15 +37,14 @@ def parse_performance_counters(output):
     perf_section = perf_section_match.group(1)
 
     perf_pattern = re.compile(
-        r"(?P<value>[\d,]+)\s+(?P<metric>[\w\./_\-]+)\s+(?:\( \+\-\s*(?P<error>-?\d+\.\d+)% \))?"
+        r"(?P<counts>[\d,]+)\s+(?P<metric>[\w\./_\-]+)\s+(?:\( \+\-\s*(?P<error>-?\d+\.\d+)% \))?"
     )
 
     for match in perf_pattern.finditer(perf_section):
-        value = match.group("value").replace(",", "")
+        counts = match.group("counts").replace(",", "")
         error = match.group("error")
-        perf_results[match.group("metric")] = (
-            int(value) if value.isdigit() else None, float(error) if error is not None else None
-        )
+        perf_results[f"{match.group('metric')} (Counts)"] = int(counts) if counts.isdigit() else None
+        perf_results[f"{match.group('metric')} (% error)"] = float(error) if error is not None else None
 
     return perf_results
 
@@ -72,7 +72,16 @@ def stack_perf_outputs(file_paths: list[Path]) -> pd.DataFrame:
 
 if __name__ == "__main__":
     my_file_paths = [
-        Path(__file__).parent.parent / "data" / "perf" / "2025-03-15_17-16-44" / "crtp_fma_perf_summary.txt"
+        Path(__file__).parent.parent
+        / "data"
+        / "perf"
+        / "2025-03-15_17-16-44"
+        / "crtp_fma_perf_summary.txt",
+        Path(__file__).parent.parent
+        / "data"
+        / "perf"
+        / "2025-03-15_17-16-25"
+        / "crtp_fma_perf_summary.txt",
     ]
 
     my_dataframe = stack_perf_outputs(my_file_paths)
