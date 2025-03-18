7b8ad937dd90853df0b774ae9922cd1708e7d5f7
Duane Goodner <dmgoodner@gmail.com>
2025-03-18 14:59:22 -0600

    test enhancements (#7)
    * add core test instructions
    
    * streamline view_dfs with PerfDataAnalyzer
    
    * add environment.yml for Conda env
    
    * update gitignore
    
    * add CLI to view_dfs
    
    * clear .idea/ from git cache

--- README.md
+++ README.md
@@ -52,11 +52,12 @@ This project is designed with modularity and ease of modification in mind, makin
 - C++ compiler compatible with C++20 standard
 - git
 - cmake
+- Miniconda or Conda (optional, )
 
-#### For Detailed Profiling (Optional)
+#### For Detailed Profiling
 
-- A Linux system with `perf` installed is required to perform the same profiling that is demonstrated in later sections.
-
+- A Linux system with `perf` installed and a user with `sudo` privileges
+- Conda or Miniconda (optional, but recommended for easy creation of Python environment for running automated tests)
 
 
 ### 🔹 Building
@@ -105,6 +106,15 @@ cmake -B build -DENABLE_PROFILING=ON
 ```
 will cause the compiler flags to be `-O3 -march=native -pg`.
 
+## 🧪 Testing Core Functionality
+
+Before running benchmarks and profiling, we can verify the functionality of all core components using automated tests built with [GoogleTest](https://github.com/google/googletest). The test source code is located in `./test/core/`.  
+
+To run all tests, use the provided script:
+
+```bash
+./test/core/run_tests.sh
+```
 
 ## 🏃 Running the Benchmarks (without profiling)
 
@@ -142,7 +152,7 @@ Other Options:
   -s                  Save execution time data
 ```
 
-### 🔹 Running All Tests
+### 🔹Benchmarking all Conditions
 To run all possible combinations of polymorphism type and compute function, and save the execution time data, run:
 
 ```shell
@@ -217,9 +227,15 @@ cmake --build build
 
 ### 🔹 Automated Test Module
 
-Python module `./test/profiling/perf_tests.py` can be used to run the benchmarks and save results as both raw `.txt` files and as cleaned `.feather` files that can be easily read into a Pandas DataFrame.
+Python module `./test/profiling/perf_tests.py` can be used to run the benchmarks and save results as both raw `.txt` files and as cleaned `.feather` files that can be easily read into a Pandas DataFrame. All Python dependencies needed to run this module are specified in `environment.yml`.
 
-***For command line help, run:**
+#### Create and Activate Conda Environment
+From the project root, run:
+```shell
+conda env create -f environment.yml
+conda activate polymorphism-compare-env
+````
+Once our Conda environment is activated, can get command line help by running:
 
 ```shell
 python test/profiling/perf_tests.py --help
@@ -286,22 +302,49 @@ Number of Iterations per Run = 50000000
 [...]
 ```
 
-
 ### 🔹 Run All Tests
 
 To run all possible combinations of polymorphism type and compute function, we can run the script without passing any arguments for `-p` or `-c`: 
 ```shell
 python test/profiling/perf_tests.py
 ```
-
-### 🔹 Viewing Profiling Data
-
-All profiling results will be saved in:
+The final lines of output will tell us where the .feather files with cleaned output data are saved:
 ```
-./data/perf/<timestamp-based-directory-name>/
+...
+Building Dataframe from detailed perf output...
+DataFrame saved to /home/duane/dproj/polymorphism-compare/data/perf/2025-03-18_14-01-51/perf_detailed_runs.feather
+Building Dataframe from summary perf output...
+DataFrame saved to /home/duane/dproj/polymorphism-compare/data/perf/2025-03-18_14-01-51/perf_summary_runs.feather
 ```
 
-Each timestamped output directory will include raw .txt data produced by `perf` as well as cleaned data in two `.feather` files: `perf_detailed_runs.feather` and `perf_summary_runs.feather`. THe former is generally more useful. Each of these can be imported into Python as a Pandas Dataframe. 
+### 🔹 Profiling Data
+
+Output from the profiling runs will be saved in a timestamped directory under `./data/perf/`. The directory  will include raw .txt data produced by `perf` as well as cleaned data in two `.feather` files: `perf_detailed_runs.feather` and `perf_summary_runs.feather`. Each of these can be imported into Python as a Pandas Dataframe. For example usage, see the code in`./test/profiling/view_dfs.py`.
+
+
+#### 🔹 View Key Profiling Data
+
+Markdown-formatted tables with some of the more interesting `perf` events data can be generated using Python module `test/profiling/view_dfs.py`. Get command line help for this simple module with:
+```
+python test/profiling/view_dfs.py --help
+```
+**Output**:
+```
+usage: view_dfs.py [-h] [-d DATA_DIR]
+
+Analyze and display performance data.
+
+options:
+  -h, --help            show this help message and exit
+  -d DATA_DIR, --data_dir DATA_DIR
+                        Name of data-containing directory under ./data/perf/ (default =
+                        view_dfs.DEFAULT_DATA_DIR_NAME)
+```
+
+#### 🔹 Collecting Additional `perf` Events
+
+The specific `perf` events collected by `./test/profiling/perf_test.py` are specified in `./tests/profiling/perf_events.json`. The content of this file can be customized to collect additional `perf` events.
+
 
 ### 📊 Results
 
--- test/profiling/view_dfs.py
+++ test/profiling/view_dfs.py
@@ -1,60 +1,119 @@
-import pyarrow.feather as feather
+import argparse
+from functools import cached_property
 from pathlib import Path
+
+import pandas as pd
+import pyarrow.feather as feather
+
+
+DEFAULT_DATA_DIR_NAME = "2025-03-18_14-01-51"
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Analyze and display performance data."
+    )
+    parser.add_argument(
+        "-d",
+        "--data_dir",
+        type=str,
+        help="Name of data-containing directory under ./data/perf/"
+        "(default = view_dfs.DEFAULT_DATA_DIR_NAME)",
+    )
+    return parser.parse_args()
+
+
+class PerfDataAnalyzer:
+    def __init__(self, data_dir: Path):
+        self.data_dir = data_dir
+
+    @staticmethod
+    def sort_df(df: pd.DataFrame) -> pd.DataFrame:
+        return df.sort_values(by="Test Name", ascending=False)
+
+    @cached_property
+    def detailed_df(self) -> pd.DataFrame:
+        df = feather.read_feather(self.data_dir / "perf_detailed_runs.feather")
+        df = self.sort_df(df=df)
+        return df
+
+    @cached_property
+    def summary_df(self) -> pd.DataFrame:
+        df = feather.read_feather(self.data_dir / "perf_summary_runs.feather")
+        df = self.sort_df(df=df)
+        return df
+
+    @cached_property
+    def basic_work_df(self) -> pd.DataFrame:
+        detailed_df_copy = self.detailed_df.copy()
+        df = detailed_df_copy[
+            [
+                "Test Name",
+                "Mean Time",
+                "branches",
+                "instructions",
+                "uops_issued.any",
+            ]
+        ]
+        return df
+
+    def display_basic_work_df(self):
+        basic_work_copy = self.basic_work_df.copy()
+        print(basic_work_copy.to_markdown(index=False))
+
+    @cached_property
+    def branch_info_df(self) -> pd.DataFrame:
+        df = self.detailed_df[
+            ["Test Name", "branches", "branch-misses"]
+        ].copy()
+        df["miss_fraction"] = df["branch-misses"] / df["branches"]
+        return df
+
+    def display_branch_info_df(self):
+        branch_info_copy = self.branch_info_df.copy()
+        print(branch_info_copy.to_markdown(index=False))
+
+    @cached_property
+    def memory_info_df(self) -> pd.DataFrame:
+        df = self.detailed_df[
+            [
+                "Test Name",
+                "mem_inst_retired.all_stores",
+                "mem_inst_retired.all_loads",
+                "cache-misses",
+            ]
+        ].copy()
+
+        df["cache-miss_fraction"] = (
+            df["cache-misses"] / df["mem_inst_retired.all_loads"]
+        )
+        return df
+
+    def display_memory_info_df(self):
+        memory_info_copy = self.memory_info_df.copy()
+        print(memory_info_copy.to_markdown(index=False))
 
 
 if __name__ == "__main__":
-    data_dir = (
-        Path(__file__).parent.parent.parent / "data" / "perf" / "2025-03-17_10-32-06"
-    )
-    detailed_df = feather.read_feather(data_dir / "perf_detailed_runs.feather")
-    summary_df = feather.read_feather(data_dir / "perf_summary_runs.feather")
+    args = get_args()
 
-    summary_df.sort_values(by="Test Name", ascending=False, inplace=True)
-    detailed_df.sort_values(by="Test Name", ascending=False, inplace=True)
-
-    basic_work = detailed_df[
-        [
-            "Test Name",
-            "Mean Time",
-            "branches",
-            "instructions",
-            "uops_issued.any",
-        ]
-    ]
-
-    basic_work_formatted = basic_work.map(
-        lambda x: f"{x:.3e}" if isinstance(x, int) else x
-    )
-    print(basic_work_formatted.to_markdown(index=False))
-
-    branch_info = detailed_df[
-        ["Test Name", "branches", "branch-misses"]
-    ].copy()
-    branch_info["miss_fraction"] = (
-        branch_info["branch-misses"] / branch_info["branches"]
-    )
-    branch_info_formatted = branch_info.map(
-        lambda x: f"{x:.3e}" if isinstance(x, int | float) else x
-    )
-    print(branch_info_formatted.to_markdown(index=False))
-
-    memory_info = detailed_df[
-        [
-            "Test Name",
-            "mem_inst_retired.all_stores",
-            "mem_inst_retired.all_loads",
-            "cache-misses",
-        ]
-    ].copy()
-
-    memory_info["cache-miss_fraction"] = (
-        memory_info["cache-misses"] / memory_info["mem_inst_retired.all_loads"]
+    my_data_dir_name = (
+        args.data_dir if args.data_dir else DEFAULT_DATA_DIR_NAME
     )
 
-    memory_info_formatted = memory_info.copy()
-    memory_info_formatted[memory_info.select_dtypes(include=['number']).columns] = (
-        memory_info[memory_info.select_dtypes(include=['number']).columns]
+    my_data_dir = (
+        Path(__file__).parent.parent.parent
+        / "data"
+        / "perf"
+        / my_data_dir_name
     )
-    print(memory_info_formatted.to_markdown(index=False))
 
+    print(f"Viewing data from:\n" f"{str(my_data_dir)}")
 
+    data_analyzer = PerfDataAnalyzer(data_dir=my_data_dir)
+    print()
+    data_analyzer.display_basic_work_df()
+    print()
+    data_analyzer.display_branch_info_df()
+    print()
+    data_analyzer.display_memory_info_df()
